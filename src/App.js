b2d75395f2979da00adc02da964552dccca0a288
maharjohn99 <38468855+maharjohn99@users.noreply.github.com>
2022-06-03 09:36:08 +0545

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
 
-const Dummy_expenses = [
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  {
-    title: "Furniture",
-    amount: 999.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    title: "Apple TV", 
-    amount: 799.49, 
-    date: new Date(2021, 2, 12) },
-
-  {
-    title: "Bicycle",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    title: "Chair (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
-const App = () => {
-  const [expenses, setExpenses] = useState(Dummy_expenses);
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
-};
+}
 
 export default App;
